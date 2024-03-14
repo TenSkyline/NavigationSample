32f700577ed208caa2f3566b0a449d8a043f2a9a
TenSkyline <rayvaldokncc@gmail.com>
2024-03-14 15:28:48 +0700

    Initial commit

---
+++ app/src/main/java/com/tenskyline/navigationsample/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.tenskyline.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tenskyline.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen() {
+    val name = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "This is first Screen", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+        Button(onClick = {  }) {
+            Text(text = "Go to 2nd Screen")
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun FirstPreview() {
+    FirstScreen()
+}
